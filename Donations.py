0e35b168cda99865d41f4224d7093eebb3dfa458
jsadiga <jsadiga@gmail.com>
2015-10-15 20:21:10 -0400

    Donations_Manager
    if else

---
+++ Donations.py
@@ -0,0 +1,10 @@
+#Donations manger
+
+salary = 10000
+donation = 0
+
+if salary > 1000:
+	donation = (10/100) * salary
+	print "You can donate " + str(donation) + " this year!"
+else:
+	print "You won't be able to donate this year!"
